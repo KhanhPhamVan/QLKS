d4148d1ab3d6c26cd4f6c00a8c887fdebdf50764
Le Ngoc Cuong <lengoccuong02042004@gmail.com>
2024-11-12 10:18:35 +0700

    Đăng nhập

--- QLKS/Forms/FormLogin.cs
+++ QLKS/Forms/FormLogin.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using QLKS.Models;
+using QLKS.Models.Extensions;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,10 +14,38 @@ namespace QLKS.Forms
 {
     public partial class FormLogin : Form
     {
+        static DbContext db = new DbContext(DbContext.ConnectionType.ConfigurationManager, "DefaultConnection");
+
+        Account account;
+
+        public Account Account => account;
+
         public FormLogin()
         {
             InitializeComponent();
         }
 
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (txtUsername.Text.Length == 0 || txtPassword.Text.Length == 0 ) {
+                MessageBox.Show("Username và Password không được để trống!", Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            } else
+            {
+                Account curr = EmployeeExts.GetAccount(txtUsername.Text.Replace(" ", ""), db);
+                if (curr != null) { 
+                    if (txtPassword.Text.HashSHA256().Equals(curr.Password))
+                    {
+                        account = curr;
+                        Close();
+                    } else
+                    {
+                        MessageBox.Show("Username hoặc Password không đúng!", Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                } else
+                {
+                    MessageBox.Show("Tài khoản này không tồn tại!", Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+        }
     }
 }
--- QLKS/Program.cs
+++ QLKS/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QLKS.Forms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,12 @@ namespace QLKS
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormMainMenu());
+            FormLogin login = new FormLogin();
+            Application.Run(login);
+            if (login.Account != null)
+            {
+                Application.Run(new FormMainMenu());
+            }
         }
     }
 }
